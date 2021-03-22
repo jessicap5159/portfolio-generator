d53393b01e5014c3614798a623f6139383b02566
jessicap5159 <jessicaj5159@gmail.com>
2021-03-22 11:12:27 -0700

    user prompts and validation

--- app.js
+++ app.js
@@ -4,19 +4,50 @@ const promptUser = () => {
     {
        type:'input',
        name: 'name',
-       message: 'What is your name?' 
+       message: 'What is your name? (required)' ,
+       validate: nameInput => {
+         if (nameInput) {
+           return true;
+         } else {
+           console.log('Please enter your name!');
+           return false;
+         }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub username'
-      },
+        message: 'Enter your GitHub username (required)',
+        validate: gitHubInput => {
+          if (gitHubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+         }
+        },
+        {
+          type: 'confirm',
+          name: 'confirmAbout',
+          message: 'Would you like to enter some information about yourself for an "About" section?',
+          default: true
+        },
+        
+   
       {
         type:'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+          if (confirmAbout) {
+            return true;
+          } else {
+            return false;
+          }
+          }
+        }
 
-      }
   ]);
 };
 promptUser().then(answers => console.log(answers));
@@ -35,12 +66,28 @@ return inquirer.prompt([
   {
     type: 'input',
     name: 'name',
-    message: "What is the name of your project?"
+    message: "What is the name of your project? (required)",
+    validate: projectNameInput => {
+      if (projectNameInput) {
+        return true;
+      } else {
+        console.log('Please enter your project name!');
+        return false;
+      }
+     }
   },
   {
     type: 'input',
     name: 'description',
-    message: 'Provide a description of your project (Required)'
+    message: 'Provide a description of your project (Required)',
+    validate: projectDescInput => {
+      if (projectDescInput) {
+        return true;
+      } else {
+        console.log('Please enter a description!');
+        return false;
+      }
+     }
   },
   {
     type:'checkbox',
@@ -51,7 +98,15 @@ return inquirer.prompt([
   {
     type: 'input',
     name: 'link',
-    message: 'Enter the GitHub link to your project. (Required)'
+    message: 'Enter the GitHub link to your project. (Required)',
+    validate: gitLinkInput => {
+      if (gitLinkInput) {
+        return true;
+      } else {
+        console.log('Please enter the GitHub link to your project!');
+        return false;
+      }
+     }
   },
   {
     type: 'confirm',
