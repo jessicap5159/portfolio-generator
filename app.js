872f8cd808739df4a9df5043c921220e9de7e9cf
jessicap5159 <jessicaj5159@gmail.com>
2021-03-22 10:11:38 -0700

    caputuring user input

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-const [name, github] = profileDataArgs; 
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type:'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers=>console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name,github);
 
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// // const printProfileData = profileDataArr => {
+// //   // This...
+// //   for (let i = 0; i < profileDataArr.length; i += 1) {
+// //     console.log(profileDataArr[i]);
+// //   }
 
-//   console.log('================');
+// //   console.log('================');
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-// const generatePage = () => 'Name: Jane, Github: janehub';
-// console.log(generatePage());
-
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-// console.log(generatePage('Jane', 'janehub'));
+// //   // Is the same as this...
+// //   profileDataArr.forEach(profileItem => console.log(profileItem));
+// // };
 
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-  if (err) throw new Error(err);
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
